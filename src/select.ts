a762f18d74a67d6463c319e99de620d828abfefb
Junting <342766475@qq.com>
2022-10-13 21:53:11 +0800

    feat: CSP - 创建 select()

---
+++ src/select.ts
@@ -0,0 +1,18 @@
+/**
+ * Select
+ * @Author: Junting
+ * @Date: 2022-10-13 21:52:33
+ * @Last Modified by: Junting
+ * @Last Modified time: 2022-10-13 21:52:53
+ */
+
+
+/**
+ * select
+ * - 以 Go 语言中 select 为原型实现。
+ *   - 实现源码：https://github.com/golang/go/blob/master/src/runtime/select.go
+ *   - 实现准则：https://stackoverflow.com/questions/37021194/how-are-golang-select-statements-implemented
+ */
+export function select() {
+
+}
